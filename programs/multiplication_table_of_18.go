e657e51e2e36c511449a3c285ab44657f377c3ae
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-06 16:29:11 +0700

    feat: add Tabel Perkalian 18 (Go)

---
+++ programs/multiplication_table_of_18.go
@@ -0,0 +1,12 @@
+// multiplication_table_of_18.go
+// Tabel perkalian 18.
+
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("18 x %d = %d\n", i, 18*i)
+	}
+}
