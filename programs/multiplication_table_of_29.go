4e6cbbbe9ca567001c1db54ac7845a39289fc0e4
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-07 20:17:11 +0700

    feat: add Tabel Perkalian 29 (Go)

---
+++ programs/multiplication_table_of_29.go
@@ -0,0 +1,12 @@
+// multiplication_table_of_29.go
+// Tabel perkalian 29.
+
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("29 x %d = %d\n", i, 29*i)
+	}
+}
