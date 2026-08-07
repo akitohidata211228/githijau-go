01992caf2e3b75981efef138fe75aa11d0221cbb
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-07 19:05:11 +0700

    feat: add Tabel Perkalian 28 (Go)

---
+++ programs/multiplication_table_of_28.go
@@ -0,0 +1,12 @@
+// multiplication_table_of_28.go
+// Tabel perkalian 28.
+
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("28 x %d = %d\n", i, 28*i)
+	}
+}
