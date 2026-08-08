b69d60d474df9872741050dedd12c7151360c839
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-08 20:50:22 +0700

    feat: add Tabel Perkalian 36 (Go)

---
+++ programs/multiplication_table_of_36.go
@@ -0,0 +1,12 @@
+// multiplication_table_of_36.go
+// Tabel perkalian 36.
+
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("36 x %d = %d\n", i, 36*i)
+	}
+}
