41bbffce62847477a173d0147deb32d0bed65b93
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-12 17:57:23 +0700

    feat: add Tabel Perkalian 43 (Go)

---
+++ programs/multiplication_table_of_43.go
@@ -0,0 +1,12 @@
+// multiplication_table_of_43.go
+// Tabel perkalian 43.
+
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("43 x %d = %d\n", i, 43*i)
+	}
+}
