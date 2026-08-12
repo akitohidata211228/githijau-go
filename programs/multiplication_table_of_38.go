aab61457b3599c6f2867862b73dec2057db4e57b
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-12 13:32:03 +0700

    feat: add Tabel Perkalian 38 (Go)

---
+++ programs/multiplication_table_of_38.go
@@ -0,0 +1,12 @@
+// multiplication_table_of_38.go
+// Tabel perkalian 38.
+
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("38 x %d = %d\n", i, 38*i)
+	}
+}
