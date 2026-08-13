72a3df5ef49d5321f98fac4f89d04df146348ac5
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-13 21:24:56 +0700

    feat: add Tabel Perkalian 51 (Go)

---
+++ programs/multiplication_table_of_51.go
@@ -0,0 +1,12 @@
+// multiplication_table_of_51.go
+// Tabel perkalian 51.
+
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("51 x %d = %d\n", i, 51*i)
+	}
+}
