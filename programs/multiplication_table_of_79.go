6650613b60fe04b88f0330702ab8cceadf559a6a
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-17 16:59:44 +0700

    feat: add Tabel Perkalian 79 (Go)

---
+++ programs/multiplication_table_of_79.go
@@ -0,0 +1,12 @@
+// multiplication_table_of_79.go
+// Tabel perkalian 79.
+
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("79 x %d = %d\n", i, 79*i)
+	}
+}
