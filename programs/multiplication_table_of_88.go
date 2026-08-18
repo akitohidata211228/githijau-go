01cb80f418f531544bc5ae29140f18e47131f916
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-18 17:29:29 +0700

    feat: add Tabel Perkalian 88 (Go)

---
+++ programs/multiplication_table_of_88.go
@@ -0,0 +1,12 @@
+// multiplication_table_of_88.go
+// Tabel perkalian 88.
+
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("88 x %d = %d\n", i, 88*i)
+	}
+}
