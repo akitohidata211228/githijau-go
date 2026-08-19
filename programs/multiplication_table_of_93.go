df27e96b467107f6f0d2cbe655576dc271754e24
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-19 12:57:02 +0700

    feat: add Tabel Perkalian 93 (Go)

---
+++ programs/multiplication_table_of_93.go
@@ -0,0 +1,12 @@
+// multiplication_table_of_93.go
+// Tabel perkalian 93.
+
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("93 x %d = %d\n", i, 93*i)
+	}
+}
