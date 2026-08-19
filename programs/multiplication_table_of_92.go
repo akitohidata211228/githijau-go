a10b81fea4152b1bf67290ae681f2958270a7a4a
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-19 11:06:02 +0700

    feat: add Tabel Perkalian 92 (Go)

---
+++ programs/multiplication_table_of_92.go
@@ -0,0 +1,12 @@
+// multiplication_table_of_92.go
+// Tabel perkalian 92.
+
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("92 x %d = %d\n", i, 92*i)
+	}
+}
